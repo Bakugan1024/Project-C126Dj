0dd1d417cbb9ccc29b5dc03b3e3482e9a2eba6ad
Bakugan1024 <85323092+Bakugan1024@users.noreply.github.com>
2022-05-09 18:11:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 song = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 
-function preload() {
+function preload()
+{
     song = loadSound("music.mp3")
 }
 
@@ -19,11 +24,37 @@ function setup() {
 }
 
 
+function modelLoaded()
+{
+    console.log('Posenet Is Initialized');
+}
+
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+
+        rightWristX=results[0].pose.righttWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+    }
+}
+
+
 function draw() {
     image(video, 0, 0, 600, 500);
 }
 
 
-function play() {
+function play()
+{
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
